2558e2796e82c4a3feaf26d27129dad7190904fa
Eva Hauk Fritzell <evahfr@gmail.com>
2016-02-05 10:19:53 +0100

    enhetstester implementert

--- java/Oblig2/HylleDemo.java
+++ java/Oblig2/HylleDemo.java
@@ -1,20 +1,69 @@
 public class HylleDemo {
     public static void main(String[] args) {
-	Hylle bokhylle = new Hylle(99);
-	Bok bok1 = new Bok("Fysikkbok", "Fysiker");
-	Bok bok2 = new Bok("Matematikkbok", "Matematiker");
 
-	System.out.println("Bokhylla har plass til " + (bokhylle.hylleStr()+1) + " boeker");
-	bokhylle.plassLedig(100);
-	bokhylle.plassLedig(45);
-	bokhylle.settInn(bok1, 45);
-	bokhylle.settInn(bok2, 45);
-	bokhylle.taUt(45);
-	bokhylle.settInn(bok2,45);
-	bokhylle.plassLedig(45);
-	bokhylle.settInn(bok1,0);
-	bokhylle.settInn(bok1,3);
-	
-	
+	Hylle bokhylle = new Hylle(100);
+	Bok bok1 = new Bok("Moby Dick", "Herman Melville");
+	Bok bok2 = new Bok("Broedrene Loevehjerte", "Astrid Lindgren");
+	Bok bok3 = new Bok("Ronja Roeverdatter", "Astrid Lindgren");
+
+	// Siden jeg tester på samme bokhylle hver gang må jeg passe på å bruke 
+	// forskjellige plasser så ikke utfallet blir påvirket av at en bok har blitt
+	// satt inn i en tidligere test. 
+	// Kunne ha unngått dette ved å lage en ny hylle for hver test.
+	testHenteFraTom(bokhylle, 50); 
+	testPlassLedig(bokhylle, bok1, 4);
+	testUgyldigPlass(bokhylle, bok1);
+	testPlassTatt(bokhylle, bok1, bok2, 0);           
     }
+
+    public static void testPlassLedig(Hylle h, Bok bok, int plass) {
+	System.out.println("\nTester om plass (" + plass + ") er ledig etter å ha satt inn og tatt ut en bok: ");
+
+	h.settInn(bok, plass);
+	h.taUt(plass);
+
+	if (h.plassLedig(plass)) {
+	    System.out.println("Passerte");
+	}
+	else {
+	    System.out.print("Feilet");
+	}
+    }
+
+    public static void testUgyldigPlass(Hylle h, Bok bok) {	
+	System.out.println("\nTester at det ikke går å sette en bok på en plass som ikke eksisterer: ");
+
+	int ugyldigPlass = h.hylleStr();
+
+	if (!h.settInn(bok, ugyldigPlass)) {
+	    System.out.println("Passerte");
+	}
+	else {
+	    System.out.println("Feilet");
+	}
+    }
+
+    public static void testPlassTatt(Hylle h, Bok bok1, Bok bok2, int plass) {
+	System.out.println("\nTester at det ikke går å sette inn en bok på en plass som er opptatt: ");
+	
+	h.settInn(bok1, plass);
+
+	if (!h.settInn(bok2, plass)) {
+	    System.out.println("Passerte");
+	}
+	else {
+	    System.out.println("Feilet");
+	}
+    }
+
+    public static void testHenteFraTom(Hylle h, int plass) {
+	System.out.println("\nTester at det ikke går å hente ut noe fra en tom plass: ");
+
+	if (h.taUt(plass) == null) {
+	    System.out.println("Passerte");
+	}
+	else {
+	    System.out.println("Feilet");
+	}
+    }    
 }
