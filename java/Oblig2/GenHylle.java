841856c1423e464329317dfcfa6ee2851e51cb99
Eva Hauk Fritzell <evahfr@gmail.com>
2016-02-05 10:20:27 +0100

    smaa endringer

--- java/Oblig2/GenHylle.java
+++ java/Oblig2/GenHylle.java
@@ -1,6 +1,6 @@
 interface GenHylle<E> {
     public int hylleStr();
-    public void settInn(E ting, int plass);
+    public boolean settInn(E ting, int plass);
     public boolean plassLedig(int plass);
     public E taUt(int plass);
 }
