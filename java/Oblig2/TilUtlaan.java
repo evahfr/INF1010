b4180b09a6543fc6af8144453e59c3738fb2def4
Eva Hauk Fritzell <evahfr@gmail.com>
2016-02-05 10:20:51 +0100

    startet dokumentering

--- java/Oblig2/TilUtlaan.java
+++ java/Oblig2/TilUtlaan.java
@@ -1,3 +1,9 @@
+/** Objektene av alle klassene som implementerer dette grensesnittet tilordner
+ * objektet en String.
+ * @author Eva H. Fritzell
+ * versjon 5. februar 2016
+ */
+
 interface TilUtlaan {
     public void laanUt(String laaner);
     public void leverTilbake();
