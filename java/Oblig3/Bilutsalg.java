d6c564984387b9a7f666a6f970b42295881911b4
Eva Hauk Fritzell <evahfr@gmail.com>
2016-02-11 18:55:22 +0100

    med vanlig array

--- java/Oblig3/Bilutsalg.java
+++ java/Oblig3/Bilutsalg.java
@@ -1,28 +1,72 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Bilutsalg {
+
+    private static Bil lagNyBil(String[] info) {
+	return new Bil(info[1]);
+    }
+
+    private static ElBil lagNyElBil(String[] info) {
+	int batteriStr = Integer.parseInt(info[2]);
+	return new ElBil(info[1], batteriStr);
+    }
+
+    private static Fossilbil lagNyFossilbil(String[] info) {
+	double utslipp = Double.parseDouble(info[2]);
+	return new Fossilbil(info[1], utslipp);
+    }
+
+    private static Personbil lagNyPersonbil(String[] info) {
+	double utslipp = Double.parseDouble(info[2]);
+	int antPassasjerer = Integer.parseInt(info[3]);
+	return new Personbil(info[1], utslipp, antPassasjerer);
+    }
+
+    private static Lastebil lagNyLastebil(String[] info) {
+	double utslipp = Double.parseDouble(info[2]);
+	double nyttevekt = Double.parseDouble(info[3]);
+	return new Lastebil(info[1], utslipp, nyttevekt);
+    }
+    
     public static void main(String[] args) throws FileNotFoundException {
 	Scanner terminal = new Scanner(System.in);
 	System.out.println("Input fil: ");
 	String filnavn = terminal.next();
-	//System.out.println("Navnet paa fila er: " + filnavn);
-	
-	File inputFil = new File(filnavn);
-	Scanner inn = new Scanner(inputFil);
 
-	while (inn.hasNextLine()) {
-	    String linje = inn.nextLine();
-	    Scanner linjeScanner = new Scanner(linje);
-	    String bilType = linjeScanner.next();
-	    String regNummer = linjeScanner.next();
-	    
-	    if (bilType.equals(EL)) {
-		int batteriStr = linjeScanner.next();
-		ElBil 
+	Scanner innFil = new Scanner(new File(filnavn));
+	Bil[] biler = new Bil[100];
+	int teller = 0;
+
+	while (innFil.hasNextLine()) {
+	    String[] linjeInfo = innFil.nextLine().split(" ");
+
+	    if (linjeInfo[0].equals("BIL")) {
+		biler[teller++] = lagNyBil(linjeInfo);
 	    }
+	    else if (linjeInfo[0].equals("EL")) {
+		biler[teller++] = lagNyElBil(linjeInfo);
+	    }
+	    else if (linjeInfo[0].equals("FOSSIL")) {
+		biler[teller++] = lagNyFossilbil(linjeInfo);
+	    }
+	    else if (linjeInfo[0].equals("PERSONFOSSILBIL")) {
+		biler[teller++] = lagNyPersonbil(linjeInfo);
+	    }
+	    else if (linjeInfo[0].equals("LASTEBIL")) {
+		biler[teller++] = lagNyLastebil(linjeInfo);
+	    }
+	    else {
+		System.out.println("Advarsel: En linje i filen '" + filnavn + "' kunne ikke tolkes. Hopper over linjen.");
+	    }
+
 	}
-	inn.close();
+	innFil.close();
+
+	for (Bil b : biler) {
+	    System.out.println(b);
+	}
     }
 }
