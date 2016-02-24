0c815312df993b42babdefb1689f07cec749c922
Eva Hauk Fritzell <evahfr@gmail.com>
2016-02-24 21:33:16 +0100

    navn

--- java/Oblig5/ListeDemo.java
+++ java/Oblig5/ListeDemo.java
@@ -2,11 +2,13 @@ public class ListeDemo {
     public static void main(String[] args) {
 	LenkeListe<String> navneListe = new LenkeListe<String>();
 	System.out.println("Listen er tom: " + navneListe.tom());
-	navneListe.leggTil("Eva");
+	navneListe.leggTil("Ola");
 	System.out.println("Listen er tom: " + navneListe.tom());
-	navneListe.leggTil("Daniel");
-	navneListe.leggTil("Tone");
+	navneListe.leggTil("Kari");
+	navneListe.leggTil("Mona");
+	navneListe.leggTil("Hans");
 	System.out.println("Listen inneholder navnet Daniel: " + navneListe.inneholder("Daniel"));
+	//System.out.println(navneListe.fjernMinste());
 	while (!navneListe.tom()) {
 	    System.out.println(navneListe.fjernMinste());
 	}
