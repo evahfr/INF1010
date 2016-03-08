90601f3199da39443244bbe6b850090c0752b224
Eva Hauk Fritzell <evahfr@gmail.com>
2016-03-08 10:59:19 +0100

    etter iterator var implementert

--- java/Oblig6/AbstraktSortertEnkelListe.java
+++ java/Oblig6/AbstraktSortertEnkelListe.java
@@ -1,9 +1,9 @@
 public interface AbstraktSortertEnkelListe<T extends Comparable<T> & Lik> extends Iterable<T> {
 
     /**
-     * Setter inn et objekt i sortert rekkefolge (minste forst) i listen.
+     * Setter inn et objekt i sortert rekkefoelge (minste foerst) i listen.
      * @param objektet
-     * @return true hvis operasjonen var vellykket, ellers false 
+     * @return sann hvis operasjonen var vellykket, ellers usann 
      */
     public boolean settInn(T element);
 
--- java/Oblig6/SortertEnkelListe.java
+++ java/Oblig6/SortertEnkelListe.java
@@ -1,7 +1,8 @@
-import java.util.Iterator;
+import java.util.*;
 
 public class SortertEnkelListe<T extends Comparable<T> & Lik> implements AbstraktSortertEnkelListe<T> {
-    private Node foran;
+
+    private Node listeHode = null;
 
     private class Node {
 	T data;
@@ -12,21 +13,58 @@ public class SortertEnkelListe<T extends Comparable<T> & Lik> implements Abstrak
 	}
     }
 
+    public SortertEnkelListe() {
+	listeHode = new Node(null);
+    }
+
     public boolean settInn(T element) {
-	Node nyNode = new Node(element);
-	
+	Node ny = new Node(element);
 	return true;
     }
 
     public T finn(String noekkel) {
-	
+	return null;
+    }
+
+    private class ListeIterator implements Iterator<T> {
+	private Node denne = listeHode;
+	private Node forrige = listeHode;
+	private boolean denneDataHentet = false;
+
+	public boolean hasNext() {
+	    return denne.neste != null;
+	}
+
+	public T next() {
+	    if (hasNext() && forrige.neste == denne.neste) {
+		denne = denne.neste;
+		denneDataHentet = true;
+		return denne.data;
+	    }
+	    else if (hasNext() && forrige.neste == denne) {
+		forrige = denne;
+		denne = denne.neste;
+		denneDataHentet = true;
+		return denne.data;
+	    }
+	    else {
+		throw new NoSuchElementException();
+	    }
+	}
+
+	public void remove() {
+	    if (denneDataHentet) {
+		forrige.neste = denne.neste;
+		denneDataHentet = false;
+	    }
+	    else {
+		throw new IllegalStateException();
+	    }
+	}
     }
 
     public Iterator<T> iterator() {
 	return new ListeIterator();
     }
 
-    private class ListeIterator implements Iterator<T> {
-
-    }
 }
