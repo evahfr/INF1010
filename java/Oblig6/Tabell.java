0d36b8e0501d94e979ec83f6848a1a52671f1324
Eva Hauk Fritzell <evahfr@gmail.com>
2016-03-08 11:50:38 +0100

    iterator i tabell må jobbes med

--- java/Oblig6/Tabell.java
+++ java/Oblig6/Tabell.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Tabell<T> implements AbstraktTabell<T> {
     private T[] tabell;
 
@@ -17,7 +19,42 @@ public class Tabell<T> implements AbstraktTabell<T> {
 	return tabell[indeks];
     }
 
-    public void gaaIgjennom() {
-	
+    private class TabellIterator implements Iterator<T> {
+	private int indeks = -1;
+	private boolean indeksDataHentet = false;
+
+	public boolean hasNext() {
+	    if (indeks+1 >= 0 && indeks+1 < tabell.length && tabell[indeks+1] != null) {
+		return true;
+	    }
+	    else {
+		return false;
+	    }
+	}
+
+	public T next() {
+	    if (hasNext()) {
+		indeksDataHentet = true;
+		indeks ++;
+		return tabell[indeks];
+	    }
+	    else {
+		throw new NoSuchElementException();
+	    }
+	}
+
+	public void remove() {
+	    if (indeksDataHentet) {
+		tabell[indeks] = null;
+		indeksDataHentet = false;
+	    }
+	    else {
+		throw new IllegalStateException();
+	    }
+	}
+    }
+
+    public Iterator<T> iterator() {
+	return new TabellIterator();
     }
 }
