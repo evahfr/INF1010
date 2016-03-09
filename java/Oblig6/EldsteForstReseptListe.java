a7813465f7525f4e11e1e1245890bb37e3976590
Eva Hauk Fritzell <evahfr@gmail.com>
2016-03-09 11:27:17 +0100

    forste commit

---
+++ java/Oblig6/EldsteForstReseptListe.java
@@ -0,0 +1,16 @@
+public class EldsteForstReseptListe extends EnkelReseptListe {
+    
+    public void settInn(Resept nyResept) {
+	Node ny = new Node(nyResept);
+	
+	if (listeHode.neste == null) {
+	    forste = ny;
+	    siste = ny;
+	    listeHode.neste = forste;
+	}
+	else {
+	    siste.neste = ny;
+	    siste = ny;
+	}
+    } 
+}
