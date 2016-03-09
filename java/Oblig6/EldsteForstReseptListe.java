3acc87270e9990664873fd50857106161c2a5c13
Eva Hauk Fritzell <evahfr@gmail.com>
2016-03-09 13:33:50 +0100

    for naa

--- java/Oblig6/EldsteForstReseptListe.java
+++ java/Oblig6/EldsteForstReseptListe.java
@@ -3,6 +3,7 @@ public class EldsteForstReseptListe extends EnkelReseptListe {
     public void settInn(Resept nyResept) {
 	Node ny = new Node(nyResept);
 	
+	// FIFO liste.
 	if (listeHode.neste == null) {
 	    forste = ny;
 	    siste = ny;
