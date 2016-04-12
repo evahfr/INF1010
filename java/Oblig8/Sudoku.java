5cfe34e9741698b4ced0ed7000062237aadbf9d7
Eva Hauk Fritzell <evahfr@gmail.com>
2016-04-12 20:35:30 +0200

    kommentert ferdig Sudoku.java

--- java/Oblig8/Sudoku.java
+++ java/Oblig8/Sudoku.java
@@ -11,10 +11,19 @@ public class Sudoku {
     private static int antKolonner;
 
     /***************************** HJELPEMETODER ******************************/
-    public static int parseInt(String data) { //Kan sende inn en beskrivelse av hvor vi er ogsaa.
+
+    /**
+     * Oversetter en streng (String) til en tallverdi (int).
+     * Metoden avslutter programmet om ikke dette ikke lar seg gjoere.
+     *
+     * @param data   strengen som skal gjoeres om
+     * @return       tallverdien 
+     */
+    private static int parseInt(String data) { 
 
 	try {
 	    return Integer.parseInt(data);
+
 	} catch (NumberFormatException e) { 
 	    System.out.printf("Kunne ikke konvertere '%s' til int.\n", data);
 	    innFil.close();
@@ -23,24 +32,39 @@ public class Sudoku {
 	return Integer.MIN_VALUE;
     }
 
-    public static int parseInt(char tegn, int maksVerdi) { //Kan sende inn en beskrivelse av hvor vi er ogsaa.	
-	int verdi = tegnTilVerdi(tegn);
+    /**
+     * Oversetter ett tegn (char) til en tallverdi (int) ved bruk av metoden
+     * verdiTilTegn(), innenfor intervallet fra 0 til en maksverdi (int).
+     * Metoden avslutter programmet om ikke dette ikke lar seg gjoere.
+     *
+     * @param tegn        tegnet som skal gjoeres om
+     * @param maksVerdi   tallet maa vaere mindre enn denne
+     * @return            tallverdien 
+     */
+    private static int parseInt(char tegn, int maksVerdi) {
+	try {
+	    int verdi = tegnTilVerdi(tegn);
 
-	if (verdi < 0 || verdi > maksVerdi) {
-	    System.out.printf("Tall utenfor lovlig intervall %c.\n", tegn);
+	    if (verdi < 0 || verdi > maksVerdi) {
+		throw new IllegalArgumentException(String.format("Tall '%c' utenfor lovlig intervall.\n", tegn));
+	    }
+	    return verdi;
+
+	} catch (IllegalArgumentException e) {
+	    System.out.println(e.getMessage());
 	    innFil.close();
-	    System.exit(1);
+	    System.exit(1);	    
 	}
-	return verdi;
+	return Integer.MIN_VALUE;	
     }
 
     /**
-     * Oversetter et tegn (char) til en tallverdi (int)
+     * Oversetter et tegn (char) til en tallverdi (int).
      *
      * @param tegn      tegnet som skal oversettes
      * @return          verdien som tegnet tilsvarer
      */
-    public static int tegnTilVerdi(char tegn) {
+    private static int tegnTilVerdi(char tegn) {
         if (tegn == '.') {                // tom rute
             return 0;
         } else if ('1' <= tegn && tegn <= '9') {    // tegn er i [1, 9]
@@ -56,18 +80,18 @@ public class Sudoku {
         } else if ('a' <= tegn && tegn <= 'z') {    // tegn er i [a, z]
             return tegn - 'a' + 39;
         } else {                                    // tegn er ugyldig
-            return -1;
+	    throw new IllegalArgumentException(String.format("Ugyldig tegn - kan ikke gjoere om '%c' til en verdi.\n", tegn));
         }
     }
 
     /**
-     * Oversetter en tallverdi (int) til et tegn (char)
+     * Oversetter en tallverdi (int) til et tegn (char).
      *
      * @param verdi     verdien som skal oversettes
      * @param tom       tegnet som brukes for aa representere 0 (tom rute)
      * @return          tegnet som verdien tilsvarer
      */
-    public static char verdiTilTegn(int verdi, char tom) {
+    private static char verdiTilTegn(int verdi, char tom) {
         if (verdi == 0) {                           // tom
             return tom;
         } else if (1 <= verdi && verdi <= 9) {      // tegn er i [1, 9]
@@ -83,31 +107,87 @@ public class Sudoku {
         } else if (39 <= verdi && verdi <= 64) {    // tegn er i [a, z]
             return (char) (verdi + 'a' - 39);
         } else {                                    // tegn er ugyldig
-            return '?';
-	    //throw new UgyldigVerdiUnntak(verdi);    // HUSK DEFINISJON AV UNNTAKSKLASSE
+            throw new IllegalArgumentException(String.format("Ugyldig verdi - kan ikke gjoere om '%d' til et tegn.\n", verdi));
         }
     }
 
-    public static int finnRuterPerBoks(int antRader, int antKolonner) {
+    /**
+     * Finner antall ruter per enhet (Rad, Kolonne, Boks).
+     * Metoden avslutter programmet om brettet er for stort.
+     *
+     * @param antRader     antall rader i en boks
+     * @param antKolonner  antall kolonner i en boks
+     * @return             antall ryter per enhet
+     */
+    private static int finnRuterPerEnhet(int antRader, int antKolonner) {
 	int maksBrettStorrelse = 64*64;
-	int boksStorrelse = (antRader*antKolonner);
-	int brettStorrelse = boksStorrelse*boksStorrelse;
+	int antRuterPerEnhet = (antRader*antKolonner);
+	int brettStorrelse = antRuterPerEnhet*antRuterPerEnhet;
 
-	if (brettStorrelse > maksBrettStorrelse) {
-	    System.out.printf("Storrelsen paa brettet er for stort: %dx%d. Maksimum brettstorrelse er 64x64.", boksStorrelse, boksStorrelse);
+	try {
+	    if (brettStorrelse > maksBrettStorrelse) {
+		throw new IllegalArgumentException(String.format("Storrelsen paa brettet er for stort: %dx%d. Maksimum brettstorrelse er 64x64.\n", antRuterPerEnhet, antRuterPerEnhet));
+	    }
+	} catch (IllegalArgumentException e) {
+	    System.out.println(e.getMessage());
 	    innFil.close();
 	    System.exit(1);
 	}
-	return boksStorrelse;
+	return antRuterPerEnhet;
     }
 
+    /**
+     * Hjelpemetode for aa lage riktig antall horisontale streker til utskrift av brett.
+     *
+     * @param antStreker     tilsvarer antall kolonner i en boks
+     * @return               streng med horisontale streker for skille til en boks
+     */
+    private static String skrivUtHorisontaltSkille(int antStreker) {
+	String skille = "";
+
+	while (antStreker > 0) {
+	    skille += "-";
+	    skrivUtHorisontaltSkille(--antStreker);
+	}
+	return skille;
+    }
+
+
+    /**
+     * Hjelpemetode for aa lage horisontalt skille til utskrift av brett.
+     *
+     * @param antRader       antall rader i en boks
+     * @param antKolonner    antall kolonner i en boks
+     * @return               streng med horisontaleskillet til en boks
+     */
+    private static String hentHorisontaltSkille(int antRader, int antKolonner) {
+	String skille = skrivUtHorisontaltSkille(antKolonner);
+
+	for (int i = antKolonner; i < antRader*antKolonner; i++) {
+	    if (i % antKolonner == 0) {
+		skille += "+" + skrivUtHorisontaltSkille(antKolonner);
+	    }
+	}
+	skille += "\n";
+	return skille;
+    }
+
+    /**************************************************************************/
+
+    /**
+     * Leser inn sudokubrettet fra en fil gitt ved en streng med filnavnet.
+     * Alle Rute objektene opprettes og settes inn i et array i Brett objektet.
+     * Metoden avslutter programmet om det er noe feil med filformatet.
+     *
+     * @param filnavn   navnet paa fila som skal leses inn
+     */
     public static void lesFil(String filnavn) {
 	try {
 
 	    innFil = new Scanner(new File(filnavn));	
 	    antRader = parseInt(innFil.nextLine());
 	    antKolonner = parseInt(innFil.nextLine());
-	    int maksVerdi = finnRuterPerBoks(antRader, antKolonner);
+	    int maksVerdi = finnRuterPerEnhet(antRader, antKolonner);
 
 	    brettet = new Brett(antRader, antKolonner);
 	    char[] linje = new char[maksVerdi];
@@ -124,9 +204,7 @@ public class Sudoku {
 	    }
 
 	    if (!brettet.erFyltUt()) {
-		System.out.printf("Antall tegn stemmer ikke: Filen '%s' inneholder for faa tegn.\n", filnavn);
-		innFil.close();
-		System.exit(1);
+		throw new IllegalArgumentException(String.format("Antall tegn stemmer ikke: Filen '%s' inneholder for faa tegn.\n", filnavn));
 	    }
 
 	} catch (FileNotFoundException e) {
@@ -138,54 +216,47 @@ public class Sudoku {
 	} catch (IndexOutOfBoundsException e) {
 	    System.out.printf("Antall tegn stemmer ikke: Filen '%s' inneholder for mange tegn.\n", filnavn);
 	    System.exit(1);
+	} catch (IllegalArgumentException e) {
+	    System.out.println(e.getMessage());
+	    System.exit(1);
 	} finally {
 	    innFil.close();
 	}
     }
 
-    public static String skrivUtHorisontaltSkille(int n) {
-	String skille = "";
-	while (n > 0) {
-	    skille += "-";
-	    skrivUtHorisontaltSkille(--n);
-	}
-	return skille;
-    }
-
-    public static String hentHorisontaltSkille(int antRader, int antKolonner) {
-	String skille = skrivUtHorisontaltSkille(antKolonner);
-	for (int i = antKolonner; i < antRader*antKolonner; i++) {
-	    if (i % antKolonner == 0) {
-		skille += "+" + skrivUtHorisontaltSkille(antKolonner);
-	    }
-	}
-	skille += "\n";
-	return skille;
-    }
-
+    /**
+     * Setter sammen utskriften til hele sudokubrettet.
+     *
+     * @return   en streng med hele utskriften
+     */    
     public static String hentBrettutskrift() {
 	Rute[] alleRuter = brettet.hentAlleRuter();
 	String hSkille = hentHorisontaltSkille(antRader, antKolonner);
 	String brettUtskrift = "";
+	int brettLengde = antKolonner*antRader;
 
 	for (int i = 0; i < alleRuter.length; i++) {
-	    if (i % (antKolonner*antRader) == 0 && i != 0) {
+	    if (i % (brettLengde) == 0 && i != 0) {
 		brettUtskrift += "\n";
 
-		if (i % ((antKolonner*antRader)*antRader) == 0  && i != 0) {
+		if (i % ((brettLengde)*antRader) == 0) {
 		    brettUtskrift += hSkille;
 		}
+
 	    } else if (i % antKolonner == 0 && i != 0) {
 		brettUtskrift += "|";
 	    }
-
 	    brettUtskrift += Character.toString(verdiTilTegn(alleRuter[i].hentVerdi(), ' '));
 	}
 	brettUtskrift += "\n";
-
 	return brettUtskrift;	
     }
 
+    /**
+     * Skriver sudokubrettet til en fil gitt ved en streng med filnavnet.
+     *
+     * @param filnavn   navnet paa fila som skal skrives til
+     */
     public static void skrivTilFil(String filnavn) {
 	try {
 	    PrintWriter utFil = new PrintWriter(new File(filnavn));
