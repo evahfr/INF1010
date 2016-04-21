7886d94e66c6627857c9f7150d24601d0828ad60
Eva Hauk Fritzell <evahfr@hargreaves.uio.no>
2016-04-21 15:36:37 +0200

    skisse av beholder ferdig

--- java/Oblig10/SudokuBeholder.java
+++ java/Oblig10/SudokuBeholder.java
@@ -1,26 +1,52 @@
 public class SudokuBeholder {
     private Brett brettet;
     private int antallLosninger = 0;
+    private Node listeHode = null;
 
     public static final int maksAntallLagredeLosninger = 3500;
 
     public SudokuBeholder(Brett brettet) {
 	this.brettet = brettet;
+	listeHode = new Node(null);
+    }
+
+    private class Node {
+	public Node neste;
+	public Rute[] losning;
+
+	public Node(Rute[] losning) {
+	    this.losning = losning;
+	}
     }
 
     public void settInn(Rute[] losning) {
 	if (++antallLosninger >= maksAntallLagredeLosninger) {
 	    return;
 	}
+
+	Node ny = new Node(losning);
 	
-       
+	if (listeHode.neste == null) {
+	    listeHode.neste = ny;
+	    return;
+	} 
+	
+	ny.neste = listeHode.neste;
+	listeHode.neste = ny;
     }
 
     public Rute[] taUt() {
-	return null;
+	if (listeHode.neste == null) {
+	    return null;
+	}
+	
+	Node denne = listeHode.neste;
+	listeHode.neste = denne.neste;
+
+	return denne.losning;
     }
 
     public int hentAntallLosninger() {
-	return antallLosninger();
+	return antallLosninger;
     }
 }
