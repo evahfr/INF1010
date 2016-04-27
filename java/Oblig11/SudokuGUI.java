51c21dbd5469e1c59a6515ea592b5aacd99312e7
Eva Hauk Fritzell <evahfr@gmail.com>
2016-04-27 13:43:04 +0200

    har tulla litt med gridpane

--- java/Oblig11/SudokuGUI.java
+++ java/Oblig11/SudokuGUI.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.*;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.scene.control.Label;
 
 import java.io.File;
 
@@ -17,6 +18,7 @@ public class SudokuGUI extends Application {
 	Scene scene = new Scene(lerret, 1230, 800);
 
 	lerret.setLeft(hentVBoks());
+	lerret.setCenter(hentStortSudokubrett(3,3));
 
 	stage.setScene(scene);
 	stage.setTitle("Sudoku");
@@ -33,34 +35,57 @@ public class SudokuGUI extends Application {
 	filVelger.getExtensionFilters().addAll(new ExtensionFilter("Text Files", "*.txt"));
 
 	File valgtFil = filVelger.showOpenDialog(stage);
-	/*
-	if (selectedFile != null) {
-	stage.display(selectedFile);
-	}
-	*/
+
 	return valgtFil;
     }
 
     public VBox hentVBoks() {
 	VBox vboks = new VBox();
 	
-	GridPane pane = new GridPane();
+	GridPane liteBrett = hentLiteSudokubrett(3,3);
 	
 	Button knapp1 = new Button("Last inn brett");
 	Button knapp2 = new Button("Lag eget brett");
 	Button knapp3 = new Button("Avslutt");
 
-	vboks.getChildren().addAll(pane, knapp1, knapp2, knapp3);
+	vboks.getChildren().addAll(liteBrett, knapp1, knapp2, knapp3);
 	
 	return vboks;
     }
 
     public GridPane hentStortSudokubrett(int antallBoksRader, int antallBoksKolonner) {
-	return null;
+	GridPane stortBrett = new GridPane();
+	//stortBrett.setHgap(80);
+	//stortBrett.setVgap(80);
+	stortBrett.setGridLinesVisible(true);
+	
+	int brettStorrelse = antallBoksRader*antallBoksKolonner;
+	for (int j = 0; j < brettStorrelse; j++) {
+	    for (int i = 0; i < brettStorrelse; i++) {
+		StackPane rute = new StackPane();
+		rute.getChildren().add(new Label(String.format("%d,%d", i, j)));
+		stortBrett.add(rute, i, j);
+	    }
+	}	
+	return stortBrett;
     }
 
     public GridPane hentLiteSudokubrett(int antallBoksRader, int antallBoksKolonner) {
-	return null;
+	GridPane liteBrett = new GridPane();
+        liteBrett.setHgap(40);
+	liteBrett.setVgap(40);
+	liteBrett.setGridLinesVisible(true);
+
+	
+	
+	int brettStorrelse = antallBoksRader*antallBoksKolonner;
+	for (int j = 0; j < brettStorrelse; j++) {
+	    for (int i = 0; i < brettStorrelse; i++) {
+		StackPane rute = new StackPane();
+		liteBrett.add(rute, i, j);
+	    }
+	}
+	return liteBrett;
     }
 
     public static void main(String[] args) {
