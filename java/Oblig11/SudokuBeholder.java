44195808009f5437fb64c617a36727570f09d8e5
Eva Hauk Fritzell <evahfr@gmail.com>
2016-05-04 14:05:13 +0200

    prover paa dobbeltlenka liste, taUt funker ikke

--- java/Oblig11/SudokuBeholder.java
+++ java/Oblig11/SudokuBeholder.java
@@ -4,16 +4,22 @@ public class SudokuBeholder {
     private Brett brettet;
     private int antallLosninger = 0;
     private Node listeHode = null;
+    private Node listeHale = null;
+    private Node tattUt = listeHode;
 
     public static final int maksAntallLagredeLosninger = 3500;
 
     public SudokuBeholder(Brett brettet) {
 	this.brettet = brettet;
 	listeHode = new Node(null);
+	listeHale = new Node(null);
+	listeHode.neste = listeHale;
+	listeHale.forrige = listeHode;
     }
 
     private class Node {
 	public Node neste;
+	public Node forrige;
 	public int[] losning;
 
 	public Node(Rute[] rutene) {
@@ -31,7 +37,7 @@ public class SudokuBeholder {
     }
 
     public boolean erTom() {
-	return listeHode.neste == null;
+	return listeHode.neste == listeHale;
     }
 
     public int hentAntallLosninger() {
@@ -40,7 +46,7 @@ public class SudokuBeholder {
 
     /**
      * Kopierer verdiene til alle rutene i Rute[] til ett int[] i en ny node.
-     * Noden settes inn forran. Etter at 3500 losninger er satt inn blir det 
+     * Noden settes inn bak. Etter at 3500 losninger er satt inn blir det 
      * ikke tatt vare paa flere, men antallLosninger oekes med en.
      * 
      * @param losning - alle rutene til losningen 
@@ -54,31 +60,82 @@ public class SudokuBeholder {
         
 	if (erTom()) {
 	    listeHode.neste = ny;
+	    ny.neste = listeHale;
+	    ny.forrige = listeHode;
+	    listeHale.forrige = ny;
 	    return;
 	} 
-	
-	ny.neste = listeHode.neste;
-	listeHode.neste = ny;
+
+	listeHale.forrige.neste = ny;
+	ny.neste = listeHale;
+	ny.forrige = listeHale.forrige;
+	listeHale.forrige = ny;
     }
 
     /**
-     * Saa lenge det er noe i beholderen fjernes den siste noden, og returneres.
+     * Returnerer verdien til den noden etter noen som siste ble returnert.
      * @return alle verdiene til losningen
      */
     public int[] taUt() {
+
 	if (erTom()) {
 	    return null;
 	}
-	
+	System.out.println(listeHode.neste);
+	if (tattUt.neste == listeHale) {
+
+	    return null;
+	}
+
+	tattUt = tattUt.neste;
+
+	/*	
 	Node denne = listeHode;
-	
+
 	while (denne.neste.neste != null) {
 	    denne = denne.neste;
 	}
         
 	int[] ret = denne.neste.losning;
 	denne.neste = null;
-	
-	return ret;
+	*/
+	return tattUt.losning;
+    }
+
+    public int[] taUtForrige() {
+	if (erTom()) {
+	    return null;
+	}
+
+	if (finnesForrige()) {
+	    tattUt = tattUt.forrige;
+	    return tattUt.losning;
+	} else {
+	    return null;
+	}
+    }
+
+    public int[] taUtNeste() {
+	if (erTom()) {
+	    return null;
+	}
+
+	if (finnesNeste()) {
+	    tattUt = tattUt.neste;
+	    return tattUt.losning;
+
+	} else {
+	    return null;
+	}	
+    }
+
+    public boolean finnesNeste() {
+	return tattUt.neste != listeHale;
+    }
+
+    public boolean finnesForrige() {
+	return tattUt.forrige != listeHode;
     }
 }
+
+
